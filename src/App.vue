0475f1f446a43b81c5c27b81ac1ae5b7c61b52a4
zhangtongyao <zhangtongyao@momenta.ai>
2019-08-16 22:14:22 +0800

    打卡

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home></home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import home from './views/home'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    home
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+*
+  margin 0
+  padding 0
 </style>
